6ff107b59273d405fda831361efe614b24ce69e8
kevinmsavsani <kevinmsavsani@gmail.com>
2024-10-03 01:03:05 +0530

    updated path finding

--- src/containers/path-finding/index.tsx
+++ src/containers/path-finding/index.tsx
@@ -13,22 +13,8 @@ import {
   RotateCcw,
   ListRestart,
 } from 'lucide-react'
-import {
-  getNodeClassName,
-  getNodesInShortestPathOrder,
-  toggleWall,
-} from './utils'
 import { cloneDeep } from 'lodash'
-import dijkstra from '../../lib/config/path-finding/dijkstra'
 import TabsComponent from './tabs'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
-import aStar from '../../lib/config/path-finding/a-star'
-import bfs from '../../lib/config/path-finding/bfs'
 import { useParams } from 'react-router-dom'
 import config from '@/lib/config'
 
@@ -158,8 +144,39 @@ export default function PathfindingVisualizer() {
 
   const runAlgorithm = () => {
     const clonedGrid = cloneDeep(grid)
-    handleReset()
     return algorithmOption.method(clonedGrid)
+  }
+
+  const getNodesInShortestPathOrder = (finishNode: Node) => {
+    const nodesInShortestPathOrder = []
+    let currentNode: Node | null = finishNode
+    while (currentNode !== null) {
+      nodesInShortestPathOrder.unshift(currentNode)
+      currentNode = currentNode.previousNode
+    }
+    return nodesInShortestPathOrder
+  }
+  
+  const getNodeClassName = (node: Node): string => {
+    if (node.isStart) return 'bg-green-500'
+    if (node.isEnd) return 'bg-red-500'
+    if (node.isPath) return 'bg-yellow-300'
+    if (node.isVisited) return 'bg-blue-300'
+    if (node.isWall) return 'bg-gray-800'
+    return 'bg-white'
+  }
+  
+  const toggleWall = (grid: Node[][], row: number, col: number) => {
+    const newGrid = grid.slice()
+    const node = newGrid[row][col]
+    if (!node.isStart && !node.isEnd) {
+      const newNode = {
+        ...node,
+        isWall: !node.isWall,
+      }
+      newGrid[row][col] = newNode
+    }
+    return newGrid
   }
 
   const generateSteps = () => {
